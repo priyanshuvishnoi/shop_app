4d4a3f8ace1a590129edb991cf744c58bbc50469
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2021-04-26 23:48:48 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato'
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
