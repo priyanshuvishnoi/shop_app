649a9498d9c0220cfe7377d4e19d0e25244fb716
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2021-04-27 12:03:31 +0530

    using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
+import './screens/product_details_screen.dart';
+import './screens/products_overview_screen.dart';
+import './providers/product.dart';
+import './providers/products.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +12,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato'
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
+  // final String title;
+
+  // ProductDetailsScreen(this.title);
+  static const routeName = '/product-detail';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
     );
   }
 }
